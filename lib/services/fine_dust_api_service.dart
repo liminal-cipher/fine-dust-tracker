ba55c575a373a38f11508924992f62715b955314
liminal-cipher <114761291+liminal-cipher@users.noreply.github.com>
2024-05-27 15:16:17 +0900

    feat: implement KECO AIRKOREA OpenAPIs
    - Integrate KECO AIRKOREA OpenAPIs for monitoring station info -
    Retrieve air pollution data based on user location - Add UI indicators
    for fine dust levels

---
+++ lib/services/fine_dust_api_service.dart
@@ -0,0 +1,51 @@
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+import 'package:fine_dust_tracker/services/location_api_service.dart';
+import 'package:fine_dust_tracker/models/measurement_station_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<MeasurementStation?> fineDustAPIService() async {
+  var current = await locationAPIService();
+
+  if (current != null) {
+    String searchByMsrstnNameURL =
+        "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty";
+    int maxAttempts = 100;
+    int attemptCount = 0;
+
+    while (attemptCount < maxAttempts) {
+      Uri msrstnURL = Uri.parse(searchByMsrstnNameURL).replace(
+        queryParameters: {
+          'stationName': current.stationName,
+          'dataTerm': 'daily',
+          'returnType': 'xml',
+          'serviceKey': dotenv.env['FINE_DUST_API_KEY'],
+          'ver': '1.3',
+        },
+      );
+
+      final response = await http.get(msrstnURL);
+      if (response.statusCode == 200) {
+        final responseData = xml.XmlDocument.parse(response.body);
+        final firstItem = responseData.findAllElements('item').firstOrNull;
+
+        if (firstItem != null) {
+          final pm10ValueElement =
+              firstItem.findAllElements('pm10Value').single.innerText;
+          final pm25ValueElement =
+              firstItem.findAllElements('pm25Value').single.innerText;
+          final dataTimeElement =
+              firstItem.findAllElements('dataTime').single.innerText;
+
+          current.pm10Value = int.tryParse(pm10ValueElement) ?? 0;
+          current.pm25Value = int.tryParse(pm25ValueElement) ?? 0;
+          current.dataTime = DateTime.parse(dataTimeElement);
+
+          return current;
+        }
+      }
+    }
+    attemptCount++;
+  }
+  return null;
+}
---
+++ lib/services/location_api_service.dart
@@ -0,0 +1,99 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:proj4dart/proj4dart.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+import 'package:fine_dust_tracker/models/measurement_station_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<MeasurementStation?> locationAPIService() async {
+  var current = MeasurementStation(
+      currentAddr: '',
+      stationName: '',
+      pm10Value: 0,
+      pm25Value: 0,
+      dataTime: DateTime.now());
+
+  LocationPermission permission = await Geolocator.requestPermission();
+  if (permission == LocationPermission.denied ||
+      permission == LocationPermission.deniedForever) {
+    return null;
+  }
+
+  // Get the user's current location
+  final position = await Geolocator.getCurrentPosition(
+    desiredAccuracy: LocationAccuracy.high,
+  );
+
+  // Get the placemark information for the current location
+  setLocaleIdentifier('ko_KR');
+  List<Placemark> placemark =
+      await placemarkFromCoordinates(position.latitude, position.longitude);
+  if (placemark.isNotEmpty) {
+    if (placemark[1].thoroughfare?.isNotEmpty == true) {
+      current.currentAddr = placemark[1].thoroughfare!;
+    } else if (placemark[0].thoroughfare?.isNotEmpty == true) {
+      current.currentAddr = placemark[0].thoroughfare!;
+    } else if (placemark[1].subLocality?.isNotEmpty == true) {
+      current.currentAddr = placemark[1].subLocality!;
+    } else if (placemark[0].subLocality?.isNotEmpty == true) {
+      current.currentAddr = placemark[0].subLocality!;
+    } else {
+      current.currentAddr = '';
+    }
+  } else {
+    current.currentAddr = '';
+  }
+
+  final gps = Projection.get('EPSG:4326')!;
+  final tm = Projection.get('EPSG:2097') ??
+      Projection.add(
+        'EPSG:2097',
+        '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43',
+      );
+
+  final gpsCoord = Point(x: position.longitude, y: position.latitude);
+  final tmCoord = gps.transform(tm, gpsCoord);
+
+  const searchByTMURL =
+      "https://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList";
+
+  const maxAttempts = 100;
+  const retryDelayDuration = Duration(seconds: 2); // Retry every 2 seconds
+
+  for (var attemptCount = 0; attemptCount < maxAttempts; attemptCount++) {
+    final nearbyMsrstnURL = Uri.parse(searchByTMURL).replace(
+      queryParameters: {
+        'serviceKey': dotenv.env['LOCATION_API_KEY'],
+        'returnType': 'xml',
+        'tmX': tmCoord.x.toString(),
+        'tmY': tmCoord.y.toString(),
+      },
+    );
+    final response = await http.get(nearbyMsrstnURL);
+    if (response.statusCode == 200) {
+      try {
+        final responseData = xml.XmlDocument.parse(response.body);
+        final firstItem = responseData.findAllElements('item').firstOrNull;
+
+        if (firstItem != null) {
+          final stationNameElement =
+              firstItem.findElements('stationName').firstOrNull;
+
+          if (stationNameElement != null) {
+            final stationName = stationNameElement.innerText;
+            if (stationName.isNotEmpty) {
+              current.stationName = stationName;
+              return current;
+            }
+          }
+        }
+      } catch (e) {
+        // Errors
+      }
+    } else {
+      await Future.delayed(retryDelayDuration);
+    }
+  }
+  return null;
+}
